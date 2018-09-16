cdcdc72a3c776c54a0b29bd2addbb0a6dd4a9b93
Joel <joelpabraham@gmail.com>
2018-09-15 22:07:49 -0500

    big dick energy

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
+import video
 from flask import *
 from werkzeug.utils import secure_filename
-import moviepy.editor
 
 
 app = Flask(__name__)
@@ -36,45 +36,8 @@ def parsemp4(filename):
     if(os.path.isfile(new_file_path)):
         return send_file(new_file_path)
 
-    #print("mp4 parsing")
-    file_path = get_file_path(filename)
-    file = moviepy.editor.VideoFileClip(file_path)
-    audio = file.audio
-
-    new_audio = fetch_audio(file)
-    file_edited = file.set_audio(new_audio)
-    file_edited.write_videofile(new_file_path)
-    #print("sending")
+    video.mp4parse(get_file_path(filename), new_file_path)
     return send_file(new_file_path)
-
-def frame_anal(frame):
-    #print("frame-anal")
-    frame_sum = 0
-    pixel_count = 0.0
-    for row in frame:
-        for pixel in row:
-            frame_sum+=sum(pixel)/len(pixel)
-            pixel_count+=1.0
-    print(frame_sum, pixel_count)
-    return frame_sum/pixel_count
-
-
-def fetch_audio(file):
-    video_sum = 0
-    #print("fetching audio")
-    num_steps = 10
-    step = file.duration/num_steps
-    #print(step)
-    for t in range(0, int(file.duration), int(step)):
-        frame = file.get_frame(t)
-        video_sum+=frame_anal(frame)
-    #print("Composite video score: "+str(video_sum))
-    return generate_audio(file, video_sum)
-
-
-def generate_audio(file, video_sum):
-    #print("generating audio")
-    return file.audio
 
 
 if __name__ == '__main__':
